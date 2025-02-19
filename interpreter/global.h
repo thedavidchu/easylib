0c5b95e4c097ce0805bed27a9cc6f8ae2aaf6fe4
David Chu <thedavidchu@gmail.com>
2025-02-18 21:02:06 -0500

    Add global header

---
+++ interpreter/global.h
@@ -0,0 +1,21 @@
+#include "object.h"
+
+struct Global {
+    struct BuiltinObjectTypes builtin_types;
+    // TODO Hold the stack frames with variables.
+};
+
+int
+init_global(struct Global *const global)
+{
+    int err = 0;
+    init_builtin_object_types(&global->builtin_types);
+    return err;
+}
+
+int
+destroy_global(struct Global *const global)
+{
+    *global = (struct Global){0};
+    return 0;
+}
